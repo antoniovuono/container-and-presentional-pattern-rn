dae40a6a42d86a7127e62568c3134d1c592cb568
Antonio Vuono <antoniosvuono@icloud.com>
2024-05-31 19:39:34 -0300

    refactor: adjust test to reuse code

--- src/screens/components/Button.test.tsx
+++ src/screens/components/Button.test.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react-native';
 import { Button } from './Button';
 
+const renderButton = (isLoading: boolean) => {
+  return render(<Button title={'Entrar'} isLoading={isLoading} />);
+};
+
 describe('Button', () => {
   it('should render button correctly', () => {
-    const tree = render(<Button title={'Entrar'} isLoading={false} />).toJSON();
+    const tree = renderButton(false).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
 
   it('should be able to render a loading indicator if isLoading is true', () => {
-    render(<Button title={'Entrar'} isLoading={true} />);
+    renderButton(true);
 
     const loadingIndicatorElement = screen.getByTestId('loading-indicator');
 
