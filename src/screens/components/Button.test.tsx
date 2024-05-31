a7289f185c0ad3f915e003f84f5f413b144ffa98
Antonio Vuono <antoniosvuono@icloud.com>
2024-05-31 19:32:51 -0300

    test: button tested

--- src/screens/components/Button.test.tsx
+++ src/screens/components/Button.test.tsx
@@ -3,12 +3,10 @@ import { render, screen } from '@testing-library/react-native';
 import { Button } from './Button';
 
 describe('Button', () => {
-  it('should be able to render a button if correct title', () => {
-    render(<Button title={'Entrar'} isLoading={false} />);
+  it('should render button correctly', () => {
+    const tree = render(<Button title={'Entrar'} isLoading={false} />).toJSON();
 
-    const buttonElement = screen.getByText('Entrar');
-
-    expect(buttonElement).toBeDefined();
+    expect(tree).toMatchSnapshot();
   });
 
   it('should be able to render a loading indicator if isLoading is true', () => {
